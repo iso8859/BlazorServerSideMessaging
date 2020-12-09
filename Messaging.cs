4ba0162ebeae0b2eafebd50b3449c4fa74c807c0
Remi THOMAS <remi@xtware.com>
2020-12-09 14:36:23 +0100

    First commit

---
+++ Messaging.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp6
+{
+    public class Messaging
+    {
+        public event Func<string, Task> NewMessageEvent;
+
+        public Task NewMessage(string msg)
+        {
+            return NewMessageEvent(msg);
+        }
+    }
+}
